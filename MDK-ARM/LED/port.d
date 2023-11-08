20a4c89b2c14d19bb76460ae5abebb44406fbd5f
Apricity <626270805@qq.com>
2023-11-08 11:01:36 +0800

    test1

---
+++ MDK-ARM/LED/port.d
@@ -0,0 +1,12 @@
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/port.c
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+led\port.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+led\port.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+led\port.o: ../Core/Inc/FreeRTOSConfig.h
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+led\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
